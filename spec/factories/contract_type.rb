8654cd99afbc2c362d9fc3ec17d2d50a82c91ee2
cristian leonardo <cristianleonardo999@gmail.com>
2019-06-12 17:12:00 -0500

    change view

--- spec/factories/contract_type.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :contract_type do
-    abbreviation 'CON'
-    name 'Contratista'
-  end
-end
--- spec/factories/contractor_type.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :contractor_type do
-    abbreviation 'PJ'
-    name 'Persona Juridica'
-  end
-end
